403fe027d9d6b1fd5aebd24919f4340b786ef188
Jonathan123asd <Jonathansutanto6@gmail.com>
2026-03-02 09:22:14 +0700

    update controller landing page

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,10 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    public function home ()
+    {
+        return view('layouts.home');
+    }
     // Tampilkan halaman login
     public function showLogin()
     {
